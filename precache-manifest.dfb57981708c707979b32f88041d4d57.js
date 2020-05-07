766b5a68cd0e3c78422ca1946993c45b582938bc
PBo <sl9site@gmail.com>
2020-05-07 01:57:20 -0700

    Updates

---
+++ precache-manifest.dfb57981708c707979b32f88041d4d57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37a45cf081913e23b698d32cca655911",
+    "url": "/kanban/index.html"
+  },
+  {
+    "revision": "dc6b4674ed7dc5cc65a4",
+    "url": "/kanban/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "90b8317cbf3badd569b2",
+    "url": "/kanban/static/css/main.b88a886c.chunk.css"
+  },
+  {
+    "revision": "dc6b4674ed7dc5cc65a4",
+    "url": "/kanban/static/js/2.81699ae6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kanban/static/js/2.81699ae6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90b8317cbf3badd569b2",
+    "url": "/kanban/static/js/main.95b0c2bb.chunk.js"
+  },
+  {
+    "revision": "89ce7f125e1730827032",
+    "url": "/kanban/static/js/runtime-main.5e8ca3e8.js"
+  }
+]);
